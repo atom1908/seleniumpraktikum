7908958e51a04df5a4b9be2ce3f98cb03c5aeb16
Vasilii Alekseev <alexeev-vy@outlook.com>
2022-09-05 17:23:49 +0300

    Переключил на хром

--- src/main/java/praktikum/BaseUITest.java
+++ src/main/java/praktikum/BaseUITest.java
@@ -17,7 +17,7 @@ public abstract class BaseUITest {
     public void setup() {
         WebDriverManager.chromedriver().setup();
         WebDriverManager.firefoxdriver().setup();
-        webDriver = new FirefoxDriver();
+        webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         webDriver.get("https://qa-scooter.praktikum-services.ru/");
     }
